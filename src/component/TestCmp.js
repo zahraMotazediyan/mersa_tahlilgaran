5e6706604654d9b48e35339a07e1313f66433321
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2024-01-01 15:56:57 +0330

    Import file css in component TestCmp

--- src/component/TestCmp.js
+++ src/component/TestCmp.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+////css
+import styles from "../css/TestCmp.module.css";
+
 const TestCmp = () => {
     return (
         <div className="container">
-            <h2>شرکت مرسا تحلیلگران</h2>
+            <h2 className={styles.d_h2}>شرکت مرسا تحلیلگران</h2>
         </div>
     )
 }
