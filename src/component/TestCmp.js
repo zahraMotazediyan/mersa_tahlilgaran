4fce1041eab24e8b05032bcac2f2fdb951a5c61d
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2024-01-01 15:33:07 +0330

    Create a component to display a text

---
+++ src/component/TestCmp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestCmp = () => {
+    return (
+        <div className="container">
+            <h2>شرکت مرسا تحلیلگران</h2>
+        </div>
+    )
+}
+
+export default TestCmp;
